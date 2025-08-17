9ed412d1f83da43005c359de05732bb15b8f289a
Shalini Katore <109355109+shalinikatore32@users.noreply.github.com>
2025-08-17 08:06:36 +0530

    Update index.js
    added environment variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Groq = require("groq-sdk");
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: [process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
